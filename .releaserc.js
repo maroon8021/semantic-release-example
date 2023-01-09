800df1f1e160aef25e8f906e294ffebf43a930d8
maroon8021 <okihiutat.iutim@gmail.com>
2023-01-09 14:48:28 +0900

    fix: .releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,15 @@
-{
-  "branches": ["main"],
-  "plugins": [
+module.exports = {
+  branches: ["main"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     [
       "@semantic-release/npm",
       {
-        "npmPublish": false,
-      }
-    ]
-  ]
-}
+        npmPublish: false,
+      },
+    ],
+  ],
+  npmPublish: false,
+};
