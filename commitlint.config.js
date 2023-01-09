a9fbd410828fdd7b74338f6bd62dbc8746a506ba
maroon8021 <okihiutat.iutim@gmail.com>
2023-01-09 17:57:10 +0900

    pref: check major version up

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    //"type-case": [2, "always", ["lower-case", "upper-case"]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "revert",
+        "pref",
+        // "BREAKING CHANGE", うまく動かん
+      ],
+    ],
+  },
 };
