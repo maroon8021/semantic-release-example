6b94fa81224d33d76db51a3ab1923410620ef32b
maroon8021 <okihiutat.iutim@gmail.com>
2023-01-09 16:00:38 +0900

    fix: set npm again

--- .releaserc.js
+++ .releaserc.js
@@ -3,8 +3,10 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     "@semantic-release/github",
   ],
   // publishのdisabledするために下記の設定が必要
+  publish: "@semantic-release/github",
   npmPublish: false,
 };
